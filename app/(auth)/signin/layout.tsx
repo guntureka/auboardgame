347163da97d8718b2e8d2d011c86e975099a2058
guntureka <guntureka1020@gmail.com>
2023-07-27 17:17:18 +0700

    login

--- app/(auth)/signin/layout.tsx
+++ app/(auth)/signin/layout.tsx
@@ -1,14 +1,11 @@
 import { Session } from "next-auth";
 import React, { Suspense } from "react";
 import Loading from "./loading";
-import DashNav from "@/components/dashboard/dashNav";
 
 const LoginLayout = ({ children }: { children: React.ReactNode }, session: Session) => {
   return (
     <section>
-      <Suspense fallback={<Loading />}>
-        {children}
-      </Suspense>
+      <Suspense fallback={<Loading />}>{children}</Suspense>
     </section>
   );
 };
