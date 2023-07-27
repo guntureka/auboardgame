0c2832999ee1bfae3124f907d0eaab5f8836ee6d
guntureka <guntureka1020@gmail.com>
2023-07-27 22:25:39 +0700

    category 1

---
+++ app/(auth)/api/quiz/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { Quiz } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request) => {
+  const quiz = await prisma.quiz.findMany();
+  return NextResponse.json(quiz, { status: 201 });
+};
+
+export const POST = async (request: Request) => {
+  const body: Quiz = await request.json();
+  const quiz = await prisma.quiz.create({
+    data: {
+      ...body,
+    },
+  });
+  return NextResponse.json(quiz, { status: 201 });
+};
