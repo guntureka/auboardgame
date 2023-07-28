6657e1c24b50b0696d56d33874298d81f1b33919
guntureka <guntureka1020@gmail.com>
2023-07-28 20:55:59 +0700

    push

---
+++ app/(auth)/api/player/[id]/route.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request, { params }: { params: { id: string } }) => {
+  const player = await prisma.player.findMany({
+    where: {
+      id: params.id,
+    },
+    include: {
+      quiz: true,
+    },
+  });
+
+  return NextResponse.json(player, { status: 201 });
+};
+
+export const PUT = async (request: Request, { params }: { params: { id: string } }) => {
+  const body = await request.json();
+
+  const player = await prisma.player.update({
+    where: {
+      id: params.id,
+    },
+    data: {
+      ...body,
+    },
+  });
+
+  return NextResponse.json(player, { status: 201 });
+};
+
+export const DELETE = async (request: Request, { params }: { params: { id: string } }) => {
+  const player = await prisma.player.delete({
+    where: {
+      id: params.id,
+    },
+  });
+
+  return NextResponse.json(player, { status: 201 });
+};
