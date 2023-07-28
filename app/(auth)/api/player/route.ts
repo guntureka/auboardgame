cf810b87ebb9b9ced4f08927b0fe614f53f84fda
guntureka <guntureka1020@gmail.com>
2023-07-28 16:46:48 +0700

    middleware

---
+++ app/(auth)/api/player/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request) => {
+  const player = await prisma.player.findMany({
+    include: {
+      quiz: true,
+    },
+  });
+
+  return NextResponse.json(player, { status: 201 });
+};
+
+export const POST = async (request: Request) => {
+  const body = await request.json();
+
+  const player = await prisma.player.create({
+    data: {
+      ...body,
+    },
+  });
+
+  return NextResponse.json(player, { status: 201 });
+};
