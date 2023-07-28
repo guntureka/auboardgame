e5df20a5faca86250ba2a27095c3fc78f7df8e1c
guntureka <guntureka1020@gmail.com>
2023-07-28 18:02:52 +0700

    middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -71,6 +71,7 @@ export const authOptions: NextAuthOptions = {
       return url.startsWith(baseUrl) ? url : baseUrl;
     },
   },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default authOptions;
