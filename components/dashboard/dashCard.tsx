fb9f575bf56859246463f9adadaa86ab8f1a1784
guntureka <guntureka1020@gmail.com>
2023-07-28 21:19:41 +0700

    push

--- components/dashboard/dashCard.tsx
+++ components/dashboard/dashCard.tsx
@@ -1,25 +1,7 @@
-import { Data } from "@/app/(dashboard)/dashboard/page";
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import React from "react";
 
-
-const DashboardCard = ({ data }: { data: Data[] }) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-      {data.map((item, index) => (
-        <Card key={index}>
-          <CardHeader>
-            <CardTitle>{item.name}</CardTitle>
-            <CardDescription className="h-[65px]">{item.description}</CardDescription>
-            <CardContent className="flex flex-col items-center my-auto border rounded-lg">
-              <div className=" mt-5">
-                <p className="font-bold text-2xl">{item.data}</p>
-              </div>
-            </CardContent>
-          </CardHeader>
-        </Card>
-      ))}
-    </div>
-  );
+const dashCard = () => {
+  return <div>dashCard</div>;
 };
 
-export default DashboardCard;
+export default dashCard;
