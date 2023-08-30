13d842f8c8450b215154b60c650c0724e9c6f4ad
guntureka <guntureka1020@gmail.com>
2023-08-30 14:19:02 +0700

    navbar left style

--- components/navbar/navbarRight.tsx
+++ components/navbar/navbarRight.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import React from "react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "../ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { AiOutlineMenu } from "react-icons/ai";
 import { MenuProfile } from "@/lib/menu";
 import Link from "next/link";
@@ -28,20 +20,14 @@ const NavbarRight = ({ session }: { session: any }) => {
           <AiOutlineMenu size={32} />
         </SheetTrigger>
         {session ? (
-          <SheetContent
-            side={"right"}
-            className="flex flex-col justify-start items-start w-[250px]"
-          >
+          <SheetContent side={"right"} className="flex flex-col justify-start items-start w-[250px]">
             <SheetHeader>
               <SheetTitle>Profile</SheetTitle>
             </SheetHeader>
             {MenuProfile.profile.map((item, index) => (
               <SheetDescription key={index} className="flex flex-col w-full">
                 <SheetClose asChild>
-                  <Link
-                    href={item.link}
-                    className="py-2 hover:bg-primary hover:text-white rounded hover:px-2 "
-                  >
+                  <Link href={item.link} className="py-2 hover:bg-primary hover:text-primary-foreground rounded hover:px-2 ">
                     {item.title}
                   </Link>
                 </SheetClose>
@@ -54,10 +40,7 @@ const NavbarRight = ({ session }: { session: any }) => {
               {MenuProfile.menu.map((item, index) => (
                 <SheetDescription key={index} className="flex flex-col w-full">
                   <SheetClose asChild>
-                    <Link
-                      href={item.link}
-                      className="py-2 hover:bg-primary hover:text-white rounded hover:px-2 "
-                    >
+                    <Link href={item.link} className="py-2 hover:bg-primary hover:text-primary-foreground rounded hover:px-2 ">
                       {item.title}
                     </Link>
                   </SheetClose>
@@ -65,29 +48,20 @@ const NavbarRight = ({ session }: { session: any }) => {
               ))}
             </div>
             <SheetClose asChild>
-              <Button
-                className="w-full"
-                onClick={() => signOut({ callbackUrl: "/" })}
-              >
+              <Button className="w-full" onClick={() => signOut({ callbackUrl: "/" })}>
                 Sign out
               </Button>
             </SheetClose>
           </SheetContent>
         ) : (
-          <SheetContent
-            side={"right"}
-            className="flex flex-col justify-start items-start w-[250px]"
-          >
+          <SheetContent side={"right"} className="flex flex-col justify-start items-start w-[250px]">
             <SheetHeader>
               <SheetTitle>Menu</SheetTitle>
             </SheetHeader>
             {MenuProfile.menu.map((item, index) => (
               <SheetDescription key={index} className="flex flex-col w-full">
                 <SheetClose asChild>
-                  <Link
-                    href={item.link}
-                    className="py-2 hover:bg-primary hover:text-white rounded hover:px-2 "
-                  >
+                  <Link href={item.link} className="py-2 hover:bg-primary hover:text-primary-foreground rounded hover:px-2 ">
                     {item.title}
                   </Link>
                 </SheetClose>
